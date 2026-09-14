aeae9395f5af669cd1d035961124f521a269cdaf
regina1-png <regina1@tlu.ee>
2026-09-14 22:27:41 +0300

    Update section_3.gd

--- problem-set-2/Scripts/section_3.gd
+++ problem-set-2/Scripts/section_3.gd
@@ -1,8 +1,18 @@
 extends Node
 
 # Write a for loop using range() that prints the numbers 1 through 10 (inclusive).
+for i in range(1, 11):
+	print(i)
 
 #rewrite it as a while loop
+var count = 1
+while count <= 10:
+	print(count)
+	count += 1
 
 #Given an array scores = [10, 9, 7, 10, 6], write a for loop that calculates and prints the total sum.
 var scores := [10, 9, 7, 10, 6]
+var total = 0
+for item in scores:
+	total += item
+print(total)
